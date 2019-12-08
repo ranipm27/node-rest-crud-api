d7f247e30422d3608b2456c884e9faccbb1f49a4
aninditadatta <58663507+aninditadatta@users.noreply.github.com>
2019-12-09 01:43:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 
 
 //mysql configuration
-var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'mysql.gamification.svc.cluster.local';
+var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'mysql-gamification.inmbzp8022.in.dst.ibm.com';
 var mysqlPort = process.env.OPENSHIFT_MYSQL_DB_PORT || 3306;
 var mysqlUser = 'xxuser'; //mysql username
 var mysqlPass = 'welcome1'; //mysql password
