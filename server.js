315062aacd3f29b55e0d69edb8c20c84cecfac47
ranipm27 <57614108+ranipm27@users.noreply.github.com>
2020-01-16 18:02:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
-var app = express();
+require('dotenv').config({ silent: true });
 var bodyParser = require('body-parser');
-var mysql = require("mysql");
+
+var app = express();
+
+//var port = 4000;
+//var port = process.env.PORT;
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
@@ -10,85 +15,21 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 	
-app.use('/api/v1/products',products);
-	
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 // default route
 app.get('/', function (req, res) {
-    return res.send({ error: true, message: 'hello' })
+
+    return res.send({ error: true, message: 'Welcome Page for Product Service' })
 });
 
+//import routes
+require("./app/routes/product.routes.js")(app);
 
-//mysql configuration
-var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'mysql.database-check.svc.cluster.local';
-var mysqlPort = process.env.OPENSHIFT_MYSQL_DB_PORT || 3306;
-var mysqlUser = 'xxuser'; //mysql username
-var mysqlPass = 'welcome1'; //mysql password
-var mysqlDb = 'sampledb'; //mysql database name
-
-//connection strings
-var mysqlString = 'mysql://' + mysqlUser + ':' + mysqlPass + '@' + mysqlHost + ':' + mysqlPort + '/' + mysqlDb;
-
-
-//connect to mysql
-var mysqlClient = mysql.createConnection({
-  host     : 'mysql.database-check.svc.cluster.local',
-  port     : '3306',
-  user     : 'xxuser',
-  password : 'welcome1',
-  database : 'sampledb'
- });
-mysqlClient.connect(function (err) {
-    if (err) console.log(err);
-});
-
-//MySQL is running!
-app.get('/mysql', function (req, res) {
-    mysqlClient.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-        if (err) {
-            res.send('NOT OK' + JSON.stringify(err));
-        } else {
-            res.send('OK: ' + rows[0].solution);
-        }
-    });
-});
-
-// connection configurations
-//var dbConn = mysql.createConnection({
-//    host: 'jdbc:mysql://mysql.gamification.svc.cluster.local:3306/mysql',
-//    user: 'xxuser',
-//    password: 'welcome1',
-//    database: 'sampledb'
-//});
-// connect to database
-//dbConn.connect(); 
-
-// Retrieve product with id 
-app.get('/products/:id', function (req, res) {
-    let item_number = req.params.id;
-    if (!item_number) {
-        return res.status(400).send({ error: true, message: 'Please provide product sku number' });
-    }
-    let item_number_int = parseInt(req.params.id);
-    mysqlClient.query('SELECT * FROM XXIBM_PRODUCT_SKU WHERE "Item Number" = ? ', item_number_int, function (error, results, fields) {
-        if (error) {
-                    //throw error;
-                    res.status(200).json({
-                    message:"Product Not found."
-                    });
-            }
-        //return res.send({ error: false, data: results[0], message: 'product list.' });
-        res.status(200).json({
-                    message:"Product found.",
-                    product: results
-                });
-        
-    });
-});
-
-// set port
-app.listen(port, ip);
-//app.listen(3000, function () {
-//    console.log('Node app is running on port 3000');
-//});
+app.listen(port);
 module.exports = app;
